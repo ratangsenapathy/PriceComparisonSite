2d4da5a40979111fa7b127690dd22906da9906fe
ratansen <ratangsenapathy@gmail.com>
2016-07-05 00:49:17 +0530

    Add comments

--- tutorial/server.py
+++ tutorial/server.py
@@ -1,22 +1,20 @@
 from flask import Flask
 from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+from scrapy.utils.project import get_project_settings          #this function automatically loads the setting in settings.py function
 
 
 app = Flask(__name__)
-process = CrawlerProcess(get_project_settings())
+process = CrawlerProcess(get_project_settings())          #creating a process to call a spider with needed setting
+#In settings.py, the JsonWriterPipeline has been activated by commenting out the required lines in settings.py
 
-@app.route('/hello')
+@app.route('/hello')                                  #rule to test if spider is working
 def hello_world():
     
-    process.crawl('foodplus', item = 'bread')
-    process.start()
+    process.crawl('foodplus', item = 'bread')       #schedulespider named foosplus and search for the item bread
+    process.start()                                 #start the process. Once the process is done the item searched for and the prices from foodplus.co.ke are stored in items.jl as defined in the item pipeline file called pipelines.py
     return "wow"
 
 
-@app.route('/test')
-def test_world():
-    return "little"
 
 if __name__ == '__main__':
    app.run(debug = True)
