bacd28600277e2d4006d85fdaaef1a499e98911f
ratansen <ratangsenapathy@gmail.com>
2016-07-06 23:51:21 +0530

    Adds celery integration code. But still incomplete

--- tutorial/items.jl
+++ tutorial/items.jl
@@ -1,8 +1,8 @@
 {"price": "KSh 699", "productName": "Excelvan Bluetooth 3.0 "}
 {"price": "KSh 1,199", "productName": "AEC Noise Reduction Wireless Bluetooth Stereo Headphone Headset for Smartphone Black "}
 {"price": "KSh 699", "productName": "BT-H06 Sport Bluetooth  Fashion Microphone Handsfree Stereo Sound for Android and IOS Phone Black "}
-{"price": "KSh 1,299", "productName": "Classic HD Bluetooth 3.0 Stereo Wireless Headphone for Smartphone Black "}
 {"price": "KSh 1,699", "productName": "New Sports Wireless Bluetooth Stereo Music Earphone Headphone Headset Black "}
+{"price": "KSh 1,299", "productName": "Classic HD Bluetooth 3.0 Stereo Wireless Headphone for Smartphone Black "}
 {"price": "KSh 2,479", "productName": "Syllable G600 Wireless Stereo Bluetooth  Headphone Earphone Headset Yellow "}
 {"price": "KSh 799", "productName": "BT-9902 Excelvan HD Bluetooth  Stereo Wireless Headphone LED Headset earphones Blue "}
 {"price": "KSh 1,399", "productName": "EXCELVAN Wireless Bluetooth LED Stereo Headphones YS-BT9919 Blue "}
@@ -11,43 +11,43 @@
 {"price": "KSh 1,589", "productName": "Stereo Bluetooth EDR Headset Earphone with Microphone Support Hands-free TF Card FM Radio Black "}
 {"price": "KSh 1,999", "productName": "EXCELVAN  Wireless Bluetooth LED Stereo Headphones Classic Adjustable Headsets Black "}
 {"price": "KSh 799", "productName": "LC-B30 New Sports Wireless Bluetooth Stereo Music Earphone Headphone Headset for Smartphone \u200b Semi-transparent "}
-{"price": "KSh 999", "productName": "Latest Sport Sweatproof Stylish Wireless Bluetooth Stereo with bulit-in Double MIC Headphone Earbuds Black "}
 {"price": "KSh 999", "productName": "New Wireless Portable Universal Audio Stereo Sensor Soundbox Speaker-White "}
+{"price": "KSh 999", "productName": "Latest Sport Sweatproof Stylish Wireless Bluetooth Stereo with bulit-in Double MIC Headphone Earbuds Black "}
 {"price": "KSh 999", "productName": "EXCELVAN Wireless Bluetooth Stereo Headphones with Mic/TF Card Grey "}
 {"price": "KSh 699", "productName": "S360 Sport Bluetooth  Hands Free Headphone Stereo Sound Music Headset for Android and IOS phone Black "}
 {"price": "KSh 2,299", "productName": "E27 Magic Flower Porcelain Wireless Bluetooth Bulb Stereo Speaker  for  Smart Phone PC Laptop White One size "}
-{"price": "KSh 795", "productName": "K2 Mini Wireless Bluetooth Bass Stereo Speaker Red One Size "}
 {"price": "KSh 2,399", "productName": "AUSDOM AH861 ShareMe Fashion Stereo Sound Music Couples Headphone Black with Red "}
 {"price": "KSh 2,229", "productName": "Zealot H1 Wireless Sports Earphones Stereo Headsets Bluetooth V4.1 Headphones White "}
-{"price": "KSh 726", "productName": "S28 Mini Wireless Bluetooth Bass Stereo Speaker Blue One Size "}
+{"price": "KSh 795", "productName": "K2 Mini Wireless Bluetooth Bass Stereo Speaker Red One Size "}
 {"price": "KSh 1,199", "productName": "Bluetooth Hifi Wireless Non-slip  Headphone Stereo Stabilized Sports HD Stereo Sound Black "}
 {"price": "KSh 1,699", "productName": "AUSDOM  M07S Popular Wireless Bluetooth Stereo Headphone Headset Earphone Green "}
 {"price": "KSh 499", "productName": "Syllable E1270 3.5mm super bass Stereo Fashionable Earphone Headphone White and green "}
-{"price": "KSh 3,271", "productName": "Sardine B1 Bluetooth V2.1 + EDR Stereo Speaker Hands Free LED Display Black One Size "}
-{"price": "KSh 4,999", "productName": "CKY Portable Wireless Bluetooth Rechargeable Stereo Hi-Fi Speaker Sound Box Pink One size "}
+{"price": "KSh 726", "productName": "S28 Mini Wireless Bluetooth Bass Stereo Speaker Blue One Size "}
 {"price": "KSh 469", "productName": "FX1X-HA Super Bass Stereo Headsets 3.5mm Plug Street Earphones White "}
 {"price": "KSh 999", "productName": "Uhappy S02 Wireless Sport headphone  Stereo bluetooth headset Black "}
-{"price": "KSh 1,901", "productName": "BOAS LC - 8600 Wireless Bluetooth Stereo Over Earphone Headset with Built-in Microphone Black "}
+{"price": "KSh 3,271", "productName": "Sardine B1 Bluetooth V2.1 + EDR Stereo Speaker Hands Free LED Display Black One Size "}
+{"price": "KSh 4,999", "productName": "CKY Portable Wireless Bluetooth Rechargeable Stereo Hi-Fi Speaker Sound Box Pink One size "}
 {"price": "KSh 999", "productName": "EXCELVAN Wireless Bluetooth Stereo Headphones YS-BT9902E Red "}
-{"price": "KSh 1,839", "productName": "EACH G4000 Pro Gaming Headset Stereo Sound Wired Headphone Noise Reduction with Microphone Blue "}
+{"price": "KSh 1,901", "productName": "BOAS LC - 8600 Wireless Bluetooth Stereo Over Earphone Headset with Built-in Microphone Black "}
 {"price": "KSh 1,382", "productName": "T2681 Water Resistant Triangle Bluetooth Speaker With Stereo Bathroom Music White One size "}
+{"price": "KSh 1,839", "productName": "EACH G4000 Pro Gaming Headset Stereo Sound Wired Headphone Noise Reduction with Microphone Blue "}
 {"price": "KSh 1,922", "productName": "AUSDOM AS2 Portable Bluetooth Stereo AUX Waterproof IPX3 Outdoor Speaker Black One Size "}
 {"price": "KSh 599", "productName": "Syllable E72 Fashion Stereo Sound Earphone Headphone  PC/ Tablet/ Android IOS Phone white+ fresh green "}
 {"price": "KSh 3,735", "productName": "MUSKY DY21L Multimedia Mini Bluetooth Speaker with Stereo FM AUX\u200b White One size "}
+{"price": "KSh 628", "productName": "EK - 250 Mini Portable Folding Stereo Lound Speaker White One Size "}
 {"price": "KSh 3,699", "productName": "Syllable Foldable  Wired Stereo Hifi OST Headset Headphones G05-003 Red "}
 {"price": "KSh 1,399", "productName": "Syllable A6 Sport  Wireless In-Ear Portable Earphone Bluetooth Stereo Sound Nacklace Design Red "}
 {"price": "KSh 2,399", "productName": "Awei A880BL Wireless Sports Stereo Earphones Black "}
-{"price": "KSh 628", "productName": "EK - 250 Mini Portable Folding Stereo Lound Speaker White One Size "}
-{"price": "KSh 2,359", "productName": "Somic V4 Double Moving-coil In-ear Headphones HiFi Stereo Earphones Blue "}
 {"price": "KSh 1,599", "productName": "Bluetooth Wireless Mini Stereo Speaker Black and Gray One Size "}
 {"price": "KSh 699", "productName": "Excelvan Bluetooth 3.0 Stereo Wireless Sports Headphone with Call Mic Blue "}
 {"price": "KSh 2,399", "productName": "Syllable  DJ Headphones Hifi Stereo Foldable Wired Headset Red "}
-{"price": "KSh 1,619", "productName": "Awei A920BL Smart Wireless Bluetooth 4.0 Sports Stereo Earphone Noise Reduction with Mic Golden "}
+{"price": "KSh 2,359", "productName": "Somic V4 Double Moving-coil In-ear Headphones HiFi Stereo Earphones Blue "}
 {"price": "KSh 1,599", "productName": "Sports In-Ear Wireless Bluetooth Stereo Earphone Headphone Earbuds for  Smartphone Red "}
+{"price": "KSh 1,619", "productName": "Awei A920BL Smart Wireless Bluetooth 4.0 Sports Stereo Earphone Noise Reduction with Mic Golden "}
 {"price": "KSh 1,209", "productName": "PLEXTONE PC780 Over-ear Gaming Headsets Earphones Headphones with Mic Blue and White "}
 {"price": "KSh 269", "productName": "Shoelace Style Earbuds Stereo Super Bass Headphones Built-in Microphone White "}
-{"price": "KSh 779", "productName": "PLEXTONE G10 In-Ear Professional Gaming Headset 3.5mm Jack Noise Cancelling Stereo Bass Headphones Red "}
 {"price": "KSh 170", "productName": "Pair Of Women's Openwork Stereo U Earrings Silver 4.7CM*0.7CM "}
+{"price": "KSh 779", "productName": "PLEXTONE G10 In-Ear Professional Gaming Headset 3.5mm Jack Noise Cancelling Stereo Bass Headphones Red "}
 {"price": "KSh 289", "productName": "OVLENG OV-L900MV 3.5mm Plug Stereo Headset Earphone Headphone with Mic Black "}
 {"price": "KSh 589", "productName": "PLEXTONE X37M Super Bass Stereo Headsets 3.5mm Plug In-Ear Metal Headphones Red "}
 {"price": "KSh 2,238", "productName": "Zealot B560 Foldable On-ear Wireless Stereo Bluetooth Headphones with FM Radio TF Card Slot Black "}
@@ -56,13 +56,13 @@
 {"price": "KSh 3,796", "productName": "LinK BT4809 2-in-1 Bluetooth  Stereo Audio Transmitter + Receiver Adapter Black One Size "}
 {"price": "KSh 730", "productName": "Lupus G1 Over-ear Gaming Headsets Earphones Headphones with Mic Stereo Bass for PC Games White "}
 {"price": "KSh 3,999", "productName": "MUSKY DY25 Stereo HIFI Bluetooth Speaker with MP3 FM Radio AUX Hands-free Function White One Size "}
-{"price": "KSh 399", "productName": "Retractable Foldable Over-ear Headphone with  Mic Stereo Bass for Kids Black "}
 {"price": "KSh 567", "productName": "Super Bass In-ear Earphone Stereo Headphone Knitted Cable with Microphone Black "}
+{"price": "KSh 399", "productName": "Retractable Foldable Over-ear Headphone with  Mic Stereo Bass for Kids Black "}
 {"price": "KSh 1,095", "productName": "G6 Outdoor Sport  Wireless Stereo Music Bluetooth V4.0 + EDR Headsets Earphones Blue "}
 {"price": "KSh 3,517", "productName": "Digicare Q8 HD Stereo Bluetooth Headphone  LED Indicator Headset with Car USB Charger Black with Silver "}
-{"price": "KSh 4,926", "productName": "Ausdom\u00ae M05 Wireless+ Wired Bluetooth Stereo Built-in Microphone For Music Streaming Black "}
 {"price": "KSh 4,799", "productName": "Syllable Y32 Wired Stereo Sound Headphone  Headset  Strong Bass Music Earphone with Headphone silver "}
 {"price": "KSh 3,539", "productName": "Excelvan Stereo Star Moves Automatically LED Landscape Projector Light Cool White One Size One Size "}
+{"price": "KSh 4,926", "productName": "Ausdom\u00ae M05 Wireless+ Wired Bluetooth Stereo Built-in Microphone For Music Streaming Black "}
 {"price": "KSh 1,399", "productName": "Sades SA - 701 Stereo Gaming Headset with Mic Volume Control Black "}
 {"price": "KSh 2,590", "productName": "OVLENG X8(headset for music) "}
 {"price": "KSh 1,699", "productName": "Novitouch Bluetooth Speaker  with Clock Display blue "}
@@ -77,26 +77,26 @@
 {"price": "KSh 7,400", "productName": "4 inch TFT Screen 12V Bluetooth V2.0 Car Radio Audio Stereo Rear View Camera MP5 Player USB SD AUX "}
 {"price": "KSh 1,300", "productName": "Bluetooth V4.1 Earphone HIFI In Ear Earbud Headphone Wireless Sports Stereo Headset Multi-point white "}
 {"price": "KSh 3,800", "productName": "2033 Car Audio Stereo FM Radio 12V USB SD Mp3 Player AUX with Remote Control "}
+{"price": "KSh 490", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
+{"price": "KSh 1,647", "productName": "Boombox Wireless Bluetooth "}
 {"price": "KSh 1,675", "productName": "Wireless SplashProof Bluetooth Speaker Adjustable Mount Outdoor Stereo MIC for Bicycle Bike Blue One Size "}
 {"price": "KSh 490", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
 {"price": "KSh 1,647", "productName": "Boombox Wireless Bluetooth "}
 {"price": "KSh 490", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
 {"price": "KSh 1,647", "productName": "Boombox Wireless Bluetooth "}
 {"price": "KSh 259", "productName": "RC-120 3.5mm Stereo Audio 1 Male to 2 Female Splitter Adapter with Volume Switch "}
-{"price": "KSh 490", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
+{"price": "KSh 460", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
 {"price": "KSh 1,647", "productName": "Boombox Wireless Bluetooth "}
 {"price": "KSh 1,299", "productName": "EXCELVAN Stereo Bluetooth Headphones, Wireless In-Ear Sports Earbuds with Mic Black "}
 {"price": "KSh 1,800", "productName": "Over-ear Gaming Headsets Earphones Headphones with Mic Stereo Bass LED Light for PC Games "}
 {"price": "KSh 1,880", "productName": "BOSCK watches Man of steel belt movement within the calendar "}
-{"price": "KSh 460", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
-{"price": "KSh 1,647", "productName": "Boombox Wireless Bluetooth "}
-{"price": "KSh 1,800", "productName": "PLEXTONE PC780 Stereo Gaming Headphones Headsets with Microphone LED Light "}
 {"price": "KSh 430", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
 {"price": "KSh 1,895", "productName": "Bluetooth 4.0 wireless "}
 {"price": "KSh 1,499", "productName": "EXCELVAN Stereo Bluetooth Headphones, Magnetic Wireless In-Ear Sports Earbuds with Mic Black "}
+{"price": "KSh 1,800", "productName": "PLEXTONE PC780 Stereo Gaming Headphones Headsets with Microphone LED Light "}
 {"price": "KSh 650", "productName": "EAST KNITTING F1176 Women White T shirt Two Pineapple Print T-shirt New Stereoscopic 3D Loose Shirts white S "}
 {"price": "KSh 1,599", "productName": "EXCELVAN Bluetooth Hands free Headset, Wireless Bluetooth Car Kit Hands Free Stereo Headset Black "}
-{"price": "KSh 2,700", "productName": "5983 Car Audio Stereo 12V MP3 Player Support FM Radio USB SD AUX "}
 {"price": "KSh 580", "productName": "Original Stereo Bass earphone Headphones Metal handsfree Headset 3.5mm Earbuds for all Mobile Phone white "}
 {"price": "KSh 1,799", "productName": "EXCELVAN Bluetooth Headset Mini Earbuds, Car Charger Black "}
+{"price": "KSh 2,700", "productName": "5983 Car Audio Stereo 12V MP3 Player Support FM Radio USB SD AUX "}
 {"price": "KSh 3,581", "productName": "WYHOO  Bluetooth V4.0 Wireless Stereo Speakerr / 8000mAh Mobile
